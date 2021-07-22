e84004910fb1d17e67eb15e3eed44115bf2625be
BruhTheresAMonkeInMyHouse <87806942+BruhTheresAMonkeInMyHouse@users.noreply.github.com>
2021-07-22 15:45:49 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,6 +1,15 @@
 
 //ADD YOUR FIREBASE LINKS HERE
-
+var firebaseConfig = {
+      apiKey: "AIzaSyBDghbh6jC3vzYR6H161Kabod8-bEoXYcQ",
+      authDomain: "kwitterthefaketwitter.firebaseapp.com",
+      projectId: "kwitterthefaketwitter",
+      storageBucket: "kwitterthefaketwitter.appspot.com",
+      messagingSenderId: "438275567210",
+      appId: "1:438275567210:web:af9c5099bb65e34dc42413"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
 function getData() {
       firebase.database().ref("/").on('value', function (snapshot) {
             document.getElementById("output").innerHTML = ""; snapshot.forEach(function (childSnapshot) {
